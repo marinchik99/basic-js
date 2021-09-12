cde80596528942d3f0f6842307cff65472cbdd64
marinchik99 <marina_1999@tut.by>
2021-09-12 18:10:02 +0300

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (Array.isArray(arr) === false){
+  throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  let array = [...arr];
+  for (let i = 0; i<array.length; i++){
+    if (array.includes('--discard-next') === true){
+      let a = array.indexOf('--discard-next');
+      if (array[a+2]==='--double-prev'){
+        array.splice(a,2,array[a+1]);
+      }
+      if(a === 0){
+        array.splice(a,1);
+      } else {
+        array.splice(a,2);
+      } 
+    } else if (array.includes('--double-next') === true){
+      let a = array.indexOf('--double-next');
+      if(a === array.length-1){
+        array.splice(a,1);
+      } else {
+        array.splice(a,1,array[a+1]);
+      }
+    } 
+     else if (array.includes('--discard-prev') === true){
+      let a = array.indexOf('--discard-prev');
+      if (arr[a-2]==='--double-next'){
+        array.splice(array[a-3],3,array[a-1],array[a+1]);
+      }
+      if(a === 0){
+        array.splice(a,1);
+      } else {
+          array.splice(array[a-1],1);
+      }
+    }
+   
+    else if (array.includes('--double-prev') === true){
+      let a = array.indexOf('--double-prev');
+      if(a === 0){
+        array.splice(a,1);
+      } else {
+        array.splice(a,1,array[a-1]);
+      } 
+    }  
+  }
+  return array;
 }
+
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+ if (date === null){
+   return 'Unable to determine the time of year!';
+ }
+
+ if (date.getMonth >=2 || date.getMonth <= 4 ){
+   return 'spring';}
+   else if(date.getMonth >=5 || date.getMonth <= 7 ){
+     return 'summer';
+   }
+   else if (date.getMonth >=8 || date.getMonth <= 10 ){
+     return 'autumn';
+   }
+   else {
+     return 'winter';
+   }
 }
