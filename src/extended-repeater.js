48c95fddc37a5eb9c4c10f8f25a696be96681ca7
marinchik99 <marina_1999@tut.by>
2021-09-14 10:14:38 +0300

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+
+  if (!options.separator) {
+      options.separator = '+';
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|'
+  }  
+  if (options.addition === undefined) {
+    options.addition = '';
+  }   
+  let addit = '', res = '';
+  for (let i=1; i<options.additionRepeatTimes; i++){
+    addit += options.addition+options.additionSeparator;
+  }
+   addit += options.addition;
+   for (let i=1; i<options.repeatTimes; i++){
+     res += str+addit + options.separator;
+   }
+   res += str + addit;
+   return res;
 }
