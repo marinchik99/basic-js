dc88fe11ee170d55f4e730790d59281a3e951a68
marinchik99 <marina_1999@tut.by>
2021-09-15 11:20:30 +0300

    feat: add encrypt and decrypt functions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor(direct = true) {
+    this.direct = direct;
+}
+encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+    throw new Error('Incorrect arguments!');}
+    const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let res = '';
+    let num = 0;
+    for (let i = 0; i < message.length; i++) {
+        if (str.includes(message.toUpperCase()[i])) {
+            res += str[((str.indexOf(message.toUpperCase()[i]) + str.indexOf(key.toUpperCase()[num % key.length]) + 26) % 26)];
+            num += 1;
+        } else {
+            res += message[i];
+        }
+    }
+    return this.direct ? res : res.split('').reverse().join('');
+
+}
+decrypt(encryptedMessage, key) {
+    if (encryptedMessage == undefined || key == undefined){
+    throw new Error('Incorrect arguments!');} 
+    const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let res = ''; let num = 0;   
+    for (let i = 0; i < encryptedMessage.length; i++) {
+        if (str.includes(encryptedMessage.toUpperCase()[i])) {
+            res += str[((str.indexOf(encryptedMessage.toUpperCase()[i]) - str.indexOf(key.toUpperCase()[num % key.length]) + 26) % 26)];
+            num +=1;
+        } else {
+            res += encryptedMessage[i];
+        }
+    }
+    return this.direct ? res : res.split('').reverse().join('');
+}
+
 }
